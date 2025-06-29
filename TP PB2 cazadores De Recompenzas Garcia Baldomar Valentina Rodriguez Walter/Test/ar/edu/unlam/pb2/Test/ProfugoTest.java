a123434a38c4625d72e590af2543a8530c7ed34d
Walter Rodriguez <walterpjrodriguez@gmail.com>
2025-06-29 18:41:59 -0300

    Creacion de la clase profugos
    Profugo y su test

---
+++ TP PB2 cazadores De Recompenzas Garcia Baldomar Valentina Rodriguez Walter/Test/ar/edu/unlam/pb2/Test/ProfugoTest.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.pb2.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.edu.unlam.pb2.dominio.Profugo;
+
+public class ProfugoTest {
+
+	
+}
