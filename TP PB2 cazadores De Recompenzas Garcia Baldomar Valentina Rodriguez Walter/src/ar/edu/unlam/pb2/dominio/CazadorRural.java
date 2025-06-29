c26c1321cfd621a8e59a8222bae89e33dfe57daf
valenbaldo <valen.facultad04@gmail.com>
2025-06-29 18:28:24 -0300

    Agregue clase: CazadorSigiloso y su respectivo Test

--- TP PB2 cazadores De Recompenzas Garcia Baldomar Valentina Rodriguez Walter/src/ar/edu/unlam/pb2/dominio/CazadorRural.java
+++ TP PB2 cazadores De Recompenzas Garcia Baldomar Valentina Rodriguez Walter/src/ar/edu/unlam/pb2/dominio/CazadorRural.java
@@ -3,9 +3,7 @@ package ar.edu.unlam.pb2.dominio;
 public class CazadorRural extends Cazador{
 
 	public CazadorRural(String nombre, int experienciaInicial) { 
-
 		super(nombre, experienciaInicial); 
-
 		} 
 	
 }
