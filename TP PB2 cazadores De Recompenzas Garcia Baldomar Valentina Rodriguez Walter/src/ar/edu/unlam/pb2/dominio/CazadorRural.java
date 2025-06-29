3a3e9afd9c14f24278add0e07b51230e41b93c8b
valenbaldo <valen.facultad04@gmail.com>
2025-06-29 18:23:53 -0300

    Agregue clase: CazadorRural y su respectivo Test

---
+++ TP PB2 cazadores De Recompenzas Garcia Baldomar Valentina Rodriguez Walter/src/ar/edu/unlam/pb2/dominio/CazadorRural.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.pb2.dominio;
+
+public class CazadorRural extends Cazador{
+
+	public CazadorRural(String nombre, int experienciaInicial) { 
+
+		super(nombre, experienciaInicial); 
+
+		} 
+	
+}
